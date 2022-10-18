c321729d036e4d437d448f4d853bfdb05ee376b7
Darin Berit R S <darinberitrs@gmail.com>
2022-10-18 10:08:58 +0530

    Third Change

--- iamneo_app/src/Navigationbar.js
+++ iamneo_app/src/Navigationbar.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import './Navbar.css';
 import {NavLink} from 'react-router-dom';
-import {Navbar,Nav,Button,Form} from 'react-bootstrap';
+import {Navbar,Nav} from 'react-bootstrap';
 export class Navigationbar extends Component{
     render(){
         return(
@@ -11,8 +11,12 @@ export class Navigationbar extends Component{
                     <Nav className="navbar navbar-expand-sm bg-light navbar-light">
                         <ul className="navbar-nav">
                             <li className="nav-item- m-1">
-                                <NavLink className="btn btn-light btn-outline-primary" to='/home'>
-                                    Home
+                                <NavLink className="btn btn-light btn-outline-info  " to='/home'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+  <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+                                </svg>
+                                    
                                 </NavLink>
                             </li>
                             <li className="nav-item- m-1">
